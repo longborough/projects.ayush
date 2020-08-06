bcee511062b379172081afe1fe7be51eb3504b69
Brent Longborough <brent@longborough.org>
2020-08-06 20:36:23 +0100

    Add top-level game outline

--- game.py
+++ game.py
@@ -1,1 +1,23 @@
 #!python
+
+def main():
+# Set up environment
+    size = 5
+    name = getName(size)
+# Play the game
+    gPlay(size, name)
+
+# gPlay is the game loop
+# When a game ends, it asks the player whether they want to pla again,
+# returning True if yes and False if no.
+def gPlay(size, name):
+    return undefined()
+
+def getName(size):
+    return undefined
+
+def undefined():
+    print("Undefined called")
+    return None
+
+main()
